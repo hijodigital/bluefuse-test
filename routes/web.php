837915d3b069e9e72ef70a39d3369f87566ec67c
hijodigital <office@hijodigital.com>
2026-05-04 13:05:51 +0100

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\NewEnquiryController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [NewEnquiryController::class, 'create'])->name('enquiry.create');
+Route::post('/enquiries', [NewEnquiryController::class, 'store'])->name('enquiry.store');
+
+Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+Route::get('/admin/enquiries/{enquiry}', [AdminController::class, 'show'])->name('admin.show');
+Route::post('/admin/enquiries/{enquiry}/status', [AdminController::class, 'updateStatus'])->name('admin.updateStatus');
